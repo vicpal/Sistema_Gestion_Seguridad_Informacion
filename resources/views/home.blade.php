8f5ec24412bb0717032f6a6c6dbf0dadc05ef037
VicPal <vjpalomonet@gmail.com>
2019-03-27 12:22:50 -0500

    Pruebas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,41 +19,5 @@
             </div>
         </div>
     </div> -->
-    <!-- Pruebas de Tablas VIP -->
-    <table class="table table-sm">
-        <thead>
-            <tr>
-                <th scope="col">Item</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Apellido</th>
-                <th scope="col">Empresa</th>
-                <th scope="col">Cargo</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>System-1</td>
-                    <td>Gestor de Calidad</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Aux. Auditoria</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                    <td>Larry the Bird</td>
-                    <td>Nunce</td>
-                    <td>@twitter</td>
-                    <td>Ingeniero</td>
-            </tr>
-        </tbody>
-    </table>
-    <!--Fin de Tablas -->
 </div>
 @endsection
