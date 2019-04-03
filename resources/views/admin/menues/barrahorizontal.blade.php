2ece28b7caa33556c22493d2a66d2757334cdf6d
VicPal <vjpalomonet@gmail.com>
2019-04-03 10:19:50 -0500

    Borre todas las pruebas adicionales

--- resources/views/admin/menues/barrahorizontal.blade.php
+++
@@ -1,5 +0,0 @@
-<section class="content-header">
-    <h1>
-        Panel de Administración
-    </h1>
-</section>
--- resources/views/admin/menues/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    	<div class="pull-right hidden-xs">
-     	 	<b>Version</b> 1.0.0
-    	</div>
-  	<strong>Copyright &copy; 2018-2019 <a href="http://127.0.0.1:8000/adm">VIP Corp</a>.</strong> All rights
-    reserved.
-</footer>
