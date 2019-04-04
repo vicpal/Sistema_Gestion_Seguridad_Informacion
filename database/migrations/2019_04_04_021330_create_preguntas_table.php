72d075445ed6c0de207efdc24581533760236531
VicPal <vjpalomonet@gmail.com>
2019-04-03 21:44:33 -0500

    Se crearon las migraciones para las tablas preguntas y respuestas

---
+++ database/migrations/2019_04_04_021330_create_preguntas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePreguntasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preguntas', function (Blueprint $table) {
+            $table->bigIncrements('id'); /*Esta debe ser llave Primaria */
+            $table->string('nombre_preg');
+            
+            $table->tinyint('numero_con'); /*Esta debe ser llave foranea */
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preguntas');
+    }
+}
