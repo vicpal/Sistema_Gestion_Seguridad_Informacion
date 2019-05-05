1ce9106365954c3db8e68250769109c54dcac282
VicPal <vjpalomonet@gmail.com>
2019-05-04 23:36:15 -0500

    no message

--- resources/views/sgsi/index.blade.php
+++ resources/views/sgsi/index.blade.php
@@ -8,8 +8,7 @@
             <P align="justify">
             Proporciona directrices para las normas de seguridad de la información organizacional 
             y las prácticas de gestión de la seguridad de la información, incluida la selección, la implementación y 
-            la gestión de controles, teniendo en cuenta el(los) entorno(s) d impreso, este artículo se le enviará a 
-            la dirección de envío suministrada.</p>
+            la gestión de controles, teniendo en cuenta el(los) entorno(s) impresos.</p>
         </div>
         <div class="col-sm-4">
             <img src="/img/icontec.jpg" alt="GTC-IEC/ISO 27002:2015">
