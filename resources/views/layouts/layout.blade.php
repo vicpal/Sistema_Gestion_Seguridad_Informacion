f01543e1ee6fb842bd3a5a8cb8d6a0a203a33815
VicPal <vjpalomonet@gmail.com>
2019-05-29 22:29:43 -0500

    Se agregó mas texto al index principal y la direccion de enlace al sitio index.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -31,7 +31,7 @@
 
   	<header class="main-header">
 		<!-- Logo -->
-		<a href="{{ route('dominios.index') }}" class="logo">
+		<a href="http://127.0.0.1:8000/sgsi" class="logo">
 			<!-- mini logo for sidebar mini 50x50 pixels -->
 			<span class="logo-mini"><b>SG</b>SI</span>
 			<!-- logo for regular state and mobile devices -->
--- resources/views/sgsi/index.blade.php
+++ resources/views/sgsi/index.blade.php
@@ -5,7 +5,21 @@
     <div class="row">
         <div class="col-sm-6">
             <h4><strong>GUIA TÉCNICA COLOMBIANA IEC/ISO 27002:2015</strong></h4>
-            <P align="justify">
+            <p align="justify">
+            El Instituto Colombiano de Normas Técnicas y Certificación, ICONTEC, es el organismo
+            nacional de normalización, según el Decreto 2269 de 1993.
+            ICONTEC es una entidad de carácter privado, sin ánimo de lucro, cuya Misión es fundamental
+            para brindar soporte y desarrollo al productor y protección al consumidor. Colabora con el
+            sector gubernamental y apoya al sector privado del país, para lograr ventajas competitivas en
+            los mercados interno y externo.</p>
+            <p align="justify">
+            La representación de todos los sectores involucrados en el proceso de Normalización Técnica
+            está garantizada por los Comités Técnicos y el período de Consulta Pública, este último
+            caracterizado por la participación del público en general.
+            La guía GTC-ISO/IEC 27002 fue ratificada por el Consejo Directivo de 2015-07-22.
+            Esta guía está sujeta a ser actualizada permanentemente con el objeto de que responda en
+            todo momento a las necesidades y exigencias actuales.</p>
+            <p align="justify">
             Proporciona directrices para las normas de seguridad de la información organizacional 
             y las prácticas de gestión de la seguridad de la información, incluida la selección, la implementación y 
             la gestión de controles, teniendo en cuenta el(los) entorno(s) impresos.</p>
